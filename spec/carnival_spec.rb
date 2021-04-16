44ca2018551de1d1f96ca3ac49818b51ff289cc9
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-04-16 08:38:53 -0600

    Carnival#add_ride tests

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -24,4 +24,19 @@ RSpec.describe Carnival do
       expect(jeffco_fair.rides).to eq([])
     end
   end
+
+  describe 'methods' do
+    it '#add_ride' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
+    end
+  end
 end
