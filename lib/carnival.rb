a21fceabb720406366a90acd90671bc6ace8407d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-04-16 09:32:55 -0600

    add Carnival#recommend_rides

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -9,4 +9,26 @@ class Carnival
   def add_ride(ride)
     @rides << ride
   end
+
+  def recommend_rides(attendee)
+    # interested_rides = []
+    # #iterate over @rides
+    # @rides.each do |ride|
+    #   #determine if ride name is in the attendee.interests array
+    #   attendee.interests.each do |interest|
+    #     interested_rides << ride if ride.name == interest
+    #   end
+    # end
+    # interested_rides
+
+    # @rides.find_all do |ride|
+    #   attendee.interests.any? do |interest|
+    #     ride.name == interest
+    #   end
+    # end
+
+    @rides.find_all do |ride|
+      attendee.interests.include?(ride.name)
+    end
+  end
 end
