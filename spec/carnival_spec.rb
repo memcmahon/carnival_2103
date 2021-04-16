8088871c00794648b5b64d9ad70ddc32c0f40465
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-04-16 10:10:49 -0600

    Carnival#ticket_lottery_contestants test

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -106,5 +106,29 @@ RSpec.describe Carnival do
 
       expect(jeffco_fair.attendees_by_ride_interest).to eq(expected)
     end
+
+    it '#ticket_lottery_contestants' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      bob = Attendee.new("Bob", 0)
+      sally = Attendee.new('Sally', 20)
+      johnny = Attendee.new("Johnny", 5)
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+      sally.add_interest('Bumper Cars')
+      johnny.add_interest('Bumper Cars')
+
+      jeffco_fair.admit(bob)
+      jeffco_fair.admit(sally)
+      jeffco_fair.admit(johnny)
+
+      exect(jeffco_fair.ticket_lottery_contestants(bumper_cars)).to eq([bob, johnny])
+      exect(jeffco_fair.ticket_lottery_contestants(scrambler)).to eq([])
+    end
   end
 end
