d1c089d3c88de4ca19f5a5210e24110458ea602a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2021-04-16 08:28:05 -0600

    Attendee.new tests

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  describe 'instantiation' do
+    it '::new' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee).to be_an_instance_of(Attendee)
+    end
+
+    it 'has attributes' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee.name).to eq('Bob')
+      expect(attendee.spending_money).to eq(20)
+    end
+
+    it 'starts with no interests' do
+      attendee = Attendee.new('Bob', 20)
+
+      expect(attendee.interests).to eq([])
+    end
+  end
+end
